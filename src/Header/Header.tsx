61dc7af42494efb18d26ead7a733962ef9bdb624
Duncan C <1658485+ddycai@users.noreply.github.com>
2021-07-25 12:59:12 -0700

    Install sass and add menu icon.

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu'
 import React from 'react';
 import styles from './Header.module.scss';
 
 const Header = () => (
   <header className={styles.Header}>
     <Button aria-label="Button to open the menu">
+      <MenuIcon></MenuIcon>
     </Button>
 
     <a href="." className="logo">
