310c93f5daeb1aa6f81d165807e38bf13d9142bc
cclzhang <hello@cecilezhang.com>
2021-08-08 13:00:05 -0400

    testing git merge

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './Header/Header';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import AmountDashboard from './dashboard-types/AmountDashboard/AmountDashboard';
+import DailyDashboard from './dashboard-types/DailyDashboard/DailyDashboard';
 import './App.scss';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <div className="App">
       <Header></Header>
       <Switch>
+        <Route path="/daily" component={DailyDashboard} />
+        <Route path="/amount" component={AmountDashboard} />
         <Route path="/about" component={About} />
         <Route path="/" component={Home} />
       </Switch>
