63add495068e006765d37d6c0a29fcf2238708d1
ddycai <1658485+ddycai@users.noreply.github.com>
2021-08-08 10:49:34 -0700

    Allow naming when adding a new dashboard

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,19 +1,27 @@
-import { List } from '@material-ui/core';
+import { List, ListSubheader } from '@material-ui/core';
 import { Button, Dialog, DialogTitle, ListItem, ListItemText } from '@material-ui/core';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addDashboard, RootState } from '../../store';
 import { Dashboard } from '../../UserData';
 import styles from './Home.module.scss';
 import AmountDashboard from '../../dashboard-types/AmountDashboard/AmountDashboard';
 import DailyDashboard from '../../dashboard-types/DailyDashboard/DailyDashboard';
+import { TextField } from '@material-ui/core';
 
 const Home = () => {
   const dashboards = useSelector((state: RootState) => state.userData.dashboards);
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
+  const [dashboardName, setDashboardName] = useState('');
   const handleClickOpen = () => {
     setOpen(true);
+  };
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target?.value) {
+      setDashboardName(event.target?.value);
+    }
   };
 
   const handleClose = () => {
@@ -21,7 +29,8 @@ const Home = () => {
   };
 
   const newDashboard = (kind: Dashboard["kind"]) => {
-    dispatch(addDashboard({ name: `default ${kind} dashboard`, kind }));
+    dispatch(addDashboard({ name: dashboardName, kind }));
+    setDashboardName('');
     setOpen(false);
   }
 
@@ -30,12 +39,14 @@ const Home = () => {
   return (<div className={styles.Home}>
     <h1>My Dashboards</h1>
     {dashboards.length ? <ul>{dashboardEls}</ul> : <p>No dashboards to show.</p>}
-    <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+    <Button variant="contained" color="primary" onClick={handleClickOpen}>
       Add a new dashboard
     </Button>
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Choose a dashboard type</DialogTitle>
+      <DialogTitle>Add Dashboard</DialogTitle>
+      <TextField id="dashboard-name" label="Dashboard Name" onChange={handleNameChange} />
       <List>
+        <ListSubheader>Dashboard Type</ListSubheader>
         <ListItem button onClick={() => newDashboard('amount')}>
           <ListItemText primary="Amount Dashboard" />
         </ListItem>
